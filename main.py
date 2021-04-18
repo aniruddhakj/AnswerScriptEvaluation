ebe2c0aeeda42e78cd7bd9bb3daaa0b79c6267d4
nitish <nitishvelu@gmail.com>
2021-04-18 14:56:23 +0530

    reference

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import os
-from io import StringIO
+# from io import StringIO
 from PIL import Image
 
 #saving selected image in the program directory for google API processing
 def save_uploaded_file(uploadedfile):
   with open(os.path.join("./",uploadedfile.name),"wb") as f:
      f.write(uploadedfile.getbuffer())
-  return st.success("Saved file {} in temporary Directory".format(uploadedfile.name))
+  return st.success("Selected image {}".format(uploadedfile.name))
+
 st.title("  Student Answer Evaluator")
 st.sidebar.write("# Menu")
 img_file = st.sidebar.file_uploader(label='', type=['png', 'jpg'],help="upload image to be evaluated")
